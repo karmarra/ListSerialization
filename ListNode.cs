d9a0fdd74ddc76739932da4f0f2122fbebdf3955
Gal va Tron <karmarra@ymail.com>
2019-05-30 18:51:52 +0300

    начало работы

---
+++ ListNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSerialization
+{
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+
+    }
+
+
+    class ListRand
+    {
+
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+        }
+
+        public void Deserialize(FileStream s)
+        {
+        }
+    }
+}
+
